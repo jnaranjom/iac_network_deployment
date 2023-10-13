2e995edf75061ca82491e9ebc1d412a9f545e489
Julian Naranjo <julian.naranjom@gmail.com>
2023-10-13 17:48:40 -0400

    Add

--- ansible_execute_save_object_attributes.sh
+++ ansible_execute_save_object_attributes.sh
@@ -2,4 +2,4 @@
 source /home/virtual_env/bin/activate
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_CONFIG=ansible.cfg
-ansible-playbook pb_ios_config.yml --skip-tags "debug"
+ansible-playbook pb_save_object_attributes.yml --skip-tags "debug"
