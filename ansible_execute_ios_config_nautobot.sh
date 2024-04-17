f9d4c3172ada9a7a70bf8d6a8192083d89619e05
Net Admin <netadmin@devserver.localdomain>
2024-04-17 03:40:02 +0000

    Use inventory plugin

--- ansible_execute_ios_config_nautobot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source /home/virtual_env/bin/activate
-export ANSIBLE_HOST_KEY_CHECKING=False
-export ANSIBLE_CONFIG=ansible.cfg
-ansible-playbook pb_get_from_nautobot.yml --skip-tags "debug"
